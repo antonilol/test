1a5c2e76660015f1169db8c47724b1005f4e5f55
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 15:50:21 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ enum Bool {
 //% color="#11bb11" block="Logic - Extra"
 namespace logicextra {
     //% block="$a $x $b"
-    export function bool(a: boolean, x: Bool, b: boolean): boolean {
+    export function bool(a: boolean, x: Bool, b: boolean) : boolean {
         return x;
     }
 }
