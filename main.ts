97006aca86499d2e403830e024cf2376cf0862b9
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 15:05:33 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,15 @@
 
 enum Bool {
-    //% block="and"
     and,
-    //% block="or"
     or,
-    //% block="nand"
     nand,
-    //% block="nor"
     nor,
-    //% block="xor"
     xor,
-    //% block="xnor"
     xnor
     
 }
 //% color="#11bb11"
-namespace test {
+namespace logic {
 
     //% block="$a $x $b"
     export function bool(a: boolean, x: Bool, b: boolean): boolean {
