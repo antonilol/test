c7cff9306f140ca93f571561c30e69c26da304ab
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 15:01:32 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,26 @@
 
+enum Bool {
+    //% block="and"
+    and,
+    //% block="or"
+    or,
+    //% block="nand"
+    nand,
+    //% block="nor"
+    nor,
+    //% block="xor"
+    xor,
+    //% block="xnor"
+    xnor
+    
+}
 //% color="#11bb11"
 namespace test {
 
-    //% block="$a and $b"
-    export function bool(a: boolean, b: boolean): boolean {
+    //% block="$a $x $b"
+    export function bool(a: boolean, x: Bool, b: boolean): boolean {
         return a;
     }
-}
-enum Choice {
-    Left,
-    Right,
-    Up,
-    Down
-}
 
-enum Choice2 {
-    //% block="left"
-    Left,
-    //% block="right"
-    Right,
-    //% block="up"
-    Up,
-    //% block="down"
-    Down
-}
-
-
-//% color="#AA278D"
-namespace basic {
-
-    //% block
-    export function turn(choice: Choice) {
-
-    }
-
-    //% block
-    export function turn2(choice: Choice2) {
-
-    }
+    
 }
