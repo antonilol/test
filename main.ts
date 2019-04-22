e018225eddedbb3d57e68089ba0682a0058f3316
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 14:32:26 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,19 @@
-//% color=190 weight=100 icon="\uf1ec" block="TEST"
-//% groups=['LED matrix', 'Control flow', 'others']
-namespace test2 {
 
-    enum Logic {
-        and = 1,
-        or = 2,
-        
-        nor = 3,
+//% color="#AA278D"
+namespace test {
+
+    //% block="fooooooo"
+    export function foo() {
+
     }
 
+    //% block="bar x = $x text = $text"
+    export function bar(x: number, text: string) {
 
-
-
-
-    //% block="magnitude of 3d vector at x %x and y %y and z %z"
-    //% inlineInputMode=inline
-    export function mag3d(x: Logic, y: number, z: number): number {
-        return Math.sqrt(x * x + y * y + z * z);
     }
-    
+
+    //% block="shuffled text = $text x = $x"
+    export function shuffled(x: number, text: string) {
+
+    }
 }
