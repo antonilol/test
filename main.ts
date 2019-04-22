c1ad0d68eb3f019573610b66c96ea2f532801d96
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 14:50:35 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,3 +7,35 @@ namespace test {
         return a;
     }
 }
+enum Choice {
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+enum Choice2 {
+    //% block="left"
+    Left,
+    //% block="right"
+    Right,
+    //% block="up"
+    Up,
+    //% block="down"
+    Down
+}
+
+
+//% color="#AA278D"
+namespace basic {
+
+    //% block
+    export function turn(choice: Choice) {
+
+    }
+
+    //% block
+    export function turn2(choice: Choice2) {
+
+    }
+}
