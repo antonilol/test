7bcb764eb7d5ff29431eb0f38dcbcc5172d2a1a5
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 14:40:24 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,9 @@
 
-//% color="#AA278D"
+//% color="#11ff11"
 namespace test {
 
-    //% block="fooooooo"
-    export function foo() {
-
-    }
-
-    //% block="bar x = $x text = $text"
-    export function bar(x: number, text: string) {
-
-    }
-
-    //% block="shuffled text = $text x = $x"
-    export function shuffled(x: number, text: string) {
-
-    }
-}
-/**
- * Organize your blocks in groups
- */
-//% color="#AA278D"
-//% groups="['Fruits', 'Veggies']"
-namespace food {
-    //% block
-    //% group="Veggies"
-    export function potato() {
-
-    }
-
-    //% block
-    //% group="Veggies"
-    export function bean() {
-
-    }
-
-    //% block
-    //% group="Fruits"
-    export function apple() {
-
-    }
-
-    //% block
-    //% group="Fruits"
-    export function banana() {
-
+    //% block="$a and $b "
+    export function bool(a:boolean,b:boolean): boolean {
+        return a and b;
     }
 }
