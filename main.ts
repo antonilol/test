03e369e6f0857815424754dae3718ef9173d7717
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-22 16:01:59 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,11 +11,11 @@ enum Bool {
 namespace logicextra {
     //% block="$a $x $b"
     export function bool(a: boolean, x: Bool, b: boolean) : boolean {
-        if (x==0){
-            return a && b;
-        } else if (x==1){
-            return a || b;
-        } else if (x==2){
+        if (x==0) {
+            return (a && b);
+        } else if (x==1) {
+            return (a || b);
+        } else if (x==2) {
             return !(a && b);
         }
     }
