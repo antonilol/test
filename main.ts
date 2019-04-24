9b04ffa94ecf2719af1cd85eea43e2ad7338e4fb
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-24 15:24:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -43,9 +43,10 @@ namespace extra {
         
         return [e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,e23,e24];
     }
-    //% num.min=0 num.max=9
+    
     //% block="show 7-seg number $num"
-    export function seg7(num:number):void{
+    //% num.min=0 num.max=9
+    export function seg7(num:number) : void{
         led.plot(1,1);
     }
 }
