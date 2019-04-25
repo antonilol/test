24b013ea77d903671214d59d835b0d87f1926628
antonilol <49868160+antonilol@users.noreply.github.com>
2019-04-25 08:07:07 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -51,14 +51,16 @@ namespace extra {
      * Plot or unplot a 7-segment 1-digit number.
      */
     //% block="$mode 7-seg number $value with offset $offset"
-    //% value.min=0 value.max=5 offset.min=0 offset.max=2
+    //% value.min=0 value.max=7 offset.min=0 offset.max=2
     export function seg7(mode:PlotUnplot,value:number,offset:number) : void{
         let list: number[][] = [[[1,1,1],[1,0,1],[1,0,1],[1,0,1],[1,1,1]],
                                 [[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]],
                                 [[1,1,1],[0,0,1],[1,1,1],[1,0,0],[1,1,1]],
                                 [[1,1,1],[0,0,1],[1,1,1],[0,0,1],[1,1,1]],
                                 [[1,0,1],[1,0,1],[1,1,1],[0,0,1],[0,0,1]],
-                                [[1,1,1],[1,0,0],[1,1,1],[0,0,1],[1,1,1]]][value];
+                                [[1,1,1],[1,0,0],[1,1,1],[0,0,1],[1,1,1]],
+                                [[1,1,1],[1,0,0],[1,1,1],[1,0,1],[1,1,1]],
+                                [[1,1,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]]]][value];
         for (let y = 0; y <= 4; y++) {
             for (let x = 0; x <= 2; x++) {
                 if (list[y][x]) {
