679d53d187f332a5796532ca0d3da2301248291d
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-04 08:33:04 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ enum Bool {
     xor=4,
     xnor=5
 }
-enum PlotUnplot {
+enum Mode {
     plot=0,
     unplot=1
 }
@@ -42,17 +42,19 @@ namespace extra {
            
         }
     }
-    //% block="25-array|$e0 $e1 $e2 $e3 $e4|$e5 $e6 $e7 $e8 $e9|$e10 $e11 $e12 $e13 $e14|$e15 $e16 $e17 $e18 $e19|$e20 $e21 $e22 $e23 $e24"
-    export function array25(e0: number,e1: number,e2: number,e3: number,e4: number,e5: number,e6: number,e7: number,e8: number,e9: number,e10: number,e11: number,e12: number,e13: number,e14: number,e15: number,e16: number,e17: number,e18: number,e19: number,e20: number,e21: number,e22: number,e23: number,e24: number) : number[] {
-        
-        return [e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,e23,e24];
+    //% block="list of 25x $e"
+    export function array25(e: number) : number[] {
+        for (let i = 0; i <= 24; i++) {
+            
+        }
+        return [0];
     }
     /**
      * Plot or unplot a 7-segment 1-digit number.
      */
     //% block="$mode 7-seg number $value with offset $offset"
     //% value.min=0 value.max=9 offset.min=0 offset.max=2
-    export function seg7(mode:PlotUnplot,value:number,offset:number) : void{
+    export function seg7(mode:Mode,value:number,offset:number) : void{
         let list: number[][] = [[[1,1,1],[1,0,1],[1,0,1],[1,0,1],[1,1,1]],
                                 [[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]],
                                 [[1,1,1],[0,0,1],[1,1,1],[1,0,0],[1,1,1]],
