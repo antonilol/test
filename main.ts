dfee1272e33f93df271db58b83e26fe0cf42a6d7
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-05 11:25:37 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,8 +11,16 @@ enum Mode {
     unplot=1
 }
 //% color="#11bb11" block="Extra Functions"
+//% groups="['Functions', 'FUNctions']"
 namespace extra {
+    //% block="on event"
+    export function onEvent(handler: () => void) {
+        
+    }
+    
     //% block="$a $x $b"
+    //% group="Functions"
+
     export function op(a: boolean, x: Bool, b: boolean) : boolean {
         if (x==0) {
             return (a && b);
@@ -32,6 +40,8 @@ namespace extra {
     }
     
     //% block="list of 25 numbers $e"
+    //% group="Functions"
+
     export function array25(e: number = 1) : number[] {
         let list: number[] = [];
         for (let i = 0; i <= 24; i++) {
@@ -43,6 +53,8 @@ namespace extra {
      * Plots the given led list to the leds. Works only when list has 25 elements.
      */
     //% block="plot led list $leds"
+    //% group="Functions"
+
     export function plotLedList(leds: number[] = array25(1)) : void {
         if (leds.length==25) {
             for (let i = 0; i <= 24; i++) {
@@ -57,6 +69,8 @@ namespace extra {
      */
     //% block="$mode 7-seg number $value offset $offset"
     //% value.min=0 value.max=9 offset.min=0 offset.max=2
+    //% group="Functions"
+
     export function seg7(mode:Mode,value:number,offset:number) : void{
         let list: number[][] = [[[1,1,1],[1,0,1],[1,0,1],[1,0,1],[1,1,1]],
                                 [[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]],
@@ -87,6 +101,8 @@ namespace extra {
      * Stackable block to create extremely large blocks.
      */
     //% block="$value"
+    //% group="FUNctions"
+
     export function numb(value:number):number{
         return value;
     }
