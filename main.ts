f00b11dc2fe6120063061317b5941404e0870a9a
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-30 16:46:26 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 
-//% color="#11bb11" block="Extra Functions" weight=100 icon="☺"
+//% color="#11bb11" block="Extra Functions" weight=100 icon="👻"
 //% groups="['Returns boolean', 'Returns number', 'Returns string', 'Returns list of numbers', 'Default blocks', 'Events', 'Extra']"
 namespace extra {
 
