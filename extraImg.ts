7f911259d575d1db0fb919f5bdc195b0db78d66a
antonilol <49868160+antonilol@users.noreply.github.com>
2019-06-01 12:39:00 +0200

    Updates.

---
+++ extraImg.ts
@@ -0,0 +1,70 @@
+//% color="#11BB11" block="Extra Functions Images" weight=100 icon="\uf03e"
+namespace extraImg {
+    
+    export enum Bool {
+        and = 0,
+        or = 1,
+        nand = 2,
+        nor = 3,
+        xor = 4,
+        xnor = 5
+    }
+
+    //% block="$imgA $x $imgB" blockId=opImg
+    export function opImg(imgA: Image, x: Bool, imgB: Image): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `);
+        if (imgA && imgB){
+            let a: boolean = false;
+            let b: boolean = false;
+            for (let imgx = 0; imgx < 5; imgx++) {
+                for (let imgy = 0; imgy < 5; imgy++) {
+                    a = imgA.pixel(imgy, imgx);
+                    b = imgB.pixel(imgy, imgx);
+                    if (x == 0) {
+                        output.setPixel(imgy, imgx, a && b);
+                    } else if (x == 1) {
+                        output.setPixel(imgy, imgx, a || b);
+                    } else if (x == 2) {
+                        output.setPixel(imgy, imgx, !(a && b));
+                    } else if (x == 3) {
+                        output.setPixel(imgy, imgx, !(a || b));
+                    } else if (x == 5) {
+                        output.setPixel(imgy, imgx, (!(a) && !(b)) || (a && b));
+                    } else if (x == 4) {
+                        output.setPixel(imgy, imgx, (!(a) && b) || (a && !(b)));
+                    } else {
+                        output.setPixel(imgy, imgx, false);
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+    //% block="inverted $img" blockId=notImg
+    export function notImg(img: Image): Image {
+        let output: Image = null
+        output = images.createImage(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `);
+        if (img) {
+            for (let imgx = 0; imgx < 5; imgx++) {
+                for (let imgy = 0; imgy < 5; imgy++) {
+                    output.setPixel(imgy, imgx, !img.pixel(imgy, imgx));
+                }
+            }
+        }
+        return output;
+    }
+}
