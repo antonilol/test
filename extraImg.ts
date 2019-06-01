4286b9f9bc5a0afd636bd4f60af08b1ed8df08d6
antonilol <49868160+antonilol@users.noreply.github.com>
2019-06-01 17:23:40 +0200

    Updates.

--- extraImg.ts
+++ extraImg.ts
@@ -117,7 +117,7 @@ namespace extra_img {
     }
     /**
      * Edit a particular pixel on an image
-     * @param img eg:%sprite(myImage)
+     * @param img eg:myImage
      */
     //% block="edit image $img x $x y $y to $newvalue" blockId=editImage
     export function editImage(img: Image, x: number, y: number,newvalue:boolean): void {
