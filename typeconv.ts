c0b2db1a3b9f6981e600cd4909aff31f315f9e34
antonilol <49868160+antonilol@users.noreply.github.com>
2019-06-02 07:35:08 +0200

    Updates.

--- typeconv.ts
+++ typeconv.ts
@@ -1,10 +1,54 @@
-namespace extra.typeconv {
-    //% block="$x" blockId=boolNumb
-    export function boolNumb(x: number): boolean {
-        return x != 0
+//% color="#11BB11" block="Type conversion (Extra)" weight=100 icon=":"
+namespace extra_typeconv {
+    /**
+     * returns:
+     * > if x == 0: false
+     * > else:      true
+     * @param x eg:1
+     */
+    //% block="number to boolean $x" blockId=boolNumb
+    export function numbBool(x: number): boolean {
+        return !(!x)
     }
-    //% block="$x" blockId=numbBool
-    export function numbBool(x: boolean): number {
+    /**
+     * returns:
+     * > if x == true: 1
+     * > else:         0
+     * @param x eg:true
+     */
+    //% block="boolean to number $x" blockId=numbBool
+    export function boolNumb(x: boolean): number {
         return x ? 1 : 0
+    }
+    /**
+     * returns:
+     * > if x == '': false
+     * > else:       true
+     * @param x eg:'text'
+     */
+    //% block="string to boolean $x" blockId=stringBool
+    export function stringBool(x: string): boolean {
+        return !(!x)
+    }
+    /**
+     * returns:
+     * > if x == true: 'true'
+     * > else:         ''
+     * @param x eg:true
+     */
+    //% block="boolean to string $x" blockId=boolString
+    export function boolString(x: boolean): string {
+        return x ? 'true' : ''
+    }
+    /**
+     * returns:
+     * > if x == true: 'true'
+     * > else:         'false'
+     * NOTE: 'false' != false
+     * @param x eg:true
+     */
+    //% block="boolean to string $x" blockId=boolString2
+    export function boolString2(x: boolean): string {
+        return x ? 'true' : 'false'
     }
 }
